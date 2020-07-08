dc00abfffd9b2e1a1181b1bde384fb29e96450c0
Nick Barrett <nick@oxygem.com>
2020-07-08 10:00:19 +0100

    Bump version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pyinfra-guzzle_sphinx_theme',
-    version='0.1',
+    version='0.2',
     description='Sphinx theme used by Guzzle, with customisations for pyinfra.',
     author='Michael Dowling (modified by Nick Barrett)',
     author_email='pointlessrambler@gmail.com',
@@ -10,7 +10,7 @@ setup(
     packages=['guzzle_sphinx_theme'],
     include_package_data=True,
     install_requires=['Sphinx>1.3'],
-    license="MIT",
+    license='MIT',
     classifiers=(
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',

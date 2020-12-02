c9929ce5b8102ae8c1fef794c46aa70f7b35adef
Nick Barrett <nick@oxygem.com>
2020-12-02 16:43:45 +0000

    Release v0.10.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pyinfra-guzzle_sphinx_theme',
-    version='0.9',
+    version='0.10',
     description='Sphinx theme used by Guzzle, with customisations for pyinfra.',
     author='Michael Dowling (modified by Nick Barrett)',
     author_email='pointlessrambler@gmail.com',

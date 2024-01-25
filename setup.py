d3ab97f106a40b0712342855603cd97b106cd375
Nick Mills-Barrett <nick@oxygem.com>
2024-01-25 21:35:24 +0000

    Add jquery / release v0.16

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="pyinfra-guzzle_sphinx_theme",
-    version="0.15",
+    version="0.16",
     description="Sphinx theme used by Guzzle, with customisations for pyinfra.",
     author="Michael Dowling (modified by Nick Barrett)",
     author_email="pointlessrambler@gmail.com",

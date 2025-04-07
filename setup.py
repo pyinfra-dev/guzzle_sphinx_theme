37994102859a8674daa1fd7456f5d5b21f4ea7ad
Nick Mills-Barrett <nick@oxygem.com>
2025-04-07 21:23:07 +0100

    Remove deprecated style var / release 0.17

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="pyinfra-guzzle_sphinx_theme",
-    version="0.16",
+    version="0.17",
     description="Sphinx theme used by Guzzle, with customisations for pyinfra.",
     author="Michael Dowling (modified by Nick Barrett)",
     author_email="pointlessrambler@gmail.com",
